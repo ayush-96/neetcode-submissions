ee899f9c8d9db149f5092429901ce089f4df4aa0
Ayush Agarwal <41159085+ayush-96@users.noreply.github.com>
2026-09-24 11:53:11 +0100

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort()
+        ans = ""
+        first, last = strs[0], strs[-1]
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return ans
+            ans += first[i]
+        return ans
